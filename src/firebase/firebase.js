04ada8419889ec93cad63cf3a2e75a02633b8c0b
Hitesh Mishra <hiteshmishra708@gmail.com>
2020-06-07 19:34:44 +0530

    firebase setup

---
+++ src/firebase/firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+};
+
+export const myFirebase = firebase.initializeApp(firebaseConfig);
+const baseDb = myFirebase.firestore();
+export const db = baseDb;
